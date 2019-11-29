16c98d57c80bb4848f20253c8c1e5fe7f6c5823f
Shish <shish@shishnet.org>
2019-11-29 12:00:09 +0000

    100% test coverage, with less copy-paste

--- tests/PDOTest.php
+++ tests/PDOTest.php
@@ -15,30 +15,55 @@ class PDOTestCase extends \PHPUnit\Framework\TestCase {
 	public function testConstructor() {
 		$db = new PDO($this->dsn);
 		$this->assertNotNull($db);
-
+		return $db;
 	}
 
-	public function testExecute() {
-		$db = new PDO($this->dsn);
+	public function testConstructorWithAuth() {
+		try {
+			new PDO("test:user=foo;password=bar");
+			$this->assertTrue(False);
+		}
+		catch(\PDOException $e) {
+			// "can't find driver" is expected
+			$this->assertTrue(True);
+		}
+	}
+
+	/**
+	 * @depends testConstructor
+	 */
+	public function testBaseData(PDO $db) {
 		$db->execute("DROP TABLE IF EXISTS test");
 		$db->execute("CREATE TABLE test (id INTEGER, value TEXT)");
 		$db->execute("INSERT INTO test VALUES (1, 'hello')");
 		$db->execute("INSERT INTO test VALUES (2, 'world')");
+		$this->assertTrue(True);
+		return $db;
+	}
 
+	/**
+	 * @depends testBaseData
+	 */
+	public function testExecute(PDO $db) {
 		$val = $db->execute("SELECT * FROM test WHERE id=:id", ["id"=>2])->fetch()['value'];
 		$this->assertEquals("world", $val);
 	}
 
-	public function testBindingInt() {
-		$db = new PDO($this->dsn);
-		$db->execute("DROP TABLE IF EXISTS test");
-		$db->execute("CREATE TABLE test (id INTEGER, value TEXT)");
-		$db->execute("INSERT INTO test VALUES (1, 'hello')");
-		$db->execute("INSERT INTO test VALUES (2, 'world')");
-
+	/**
+	 * @depends testBaseData
+	 */
+	public function testBindingInt(PDO $db) {
 		// By default, mysql would bind this as the string '1' and then
 		// throw a syntax error - let's check that that doesn't happen.
 		$res = $db->execute("SELECT * FROM test LIMIT :limit", ["limit"=>1]);
+		$this->assertCount(1, $res);
+	}
+
+	/**
+	 * @depends testBaseData
+	 */
+	public function testBindingStr(PDO $db) {
+		$res = $db->execute("SELECT * FROM test WHERE value=:value", ["value"=>"hello"]);
 		$this->assertCount(1, $res);
 	}
 }
