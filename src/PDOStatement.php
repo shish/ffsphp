dbc0175c1c8ca769e01c433a9b6d64b2d3bf9857
Shish <shish@shishnet.org>
2020-10-09 15:59:09 +0100

    format

--- src/PDOStatement.php
+++ src/PDOStatement.php
@@ -2,30 +2,33 @@
 
 namespace FFSPHP;
 
-class PDOStatement extends \PDOStatement {
-	protected $PDO = null;
+class PDOStatement extends \PDOStatement
+{
+    protected $PDO = null;
 
-	protected function __construct(PDO &$PDO) {
-		$this->PDO = $PDO;
-	}
+    protected function __construct(PDO &$PDO)
+    {
+        $this->PDO = $PDO;
+    }
 
-	/**
-	 * like upstream execute(), except that integers are bound
-	 * as integers, so "LIMIT :foo" [foo=3] turns into "LIMIT 3"
-	 * instead of "LIMIT '3'"
-	 */
-	public function execute($input_parameters = null) {
-		if($input_parameters) {
-			foreach($input_parameters as $name => $value) {
+    /**
+     * like upstream execute(), except that integers are bound
+     * as integers, so "LIMIT :foo" [foo=3] turns into "LIMIT 3"
+     * instead of "LIMIT '3'"
+     */
+    public function execute($input_parameters = null)
+    {
+        if ($input_parameters) {
+            foreach ($input_parameters as $name => $value) {
                 if (is_bool($value)) {
                     $this->bindValue(':'.$name, $value, PDO::PARAM_BOOL);
                 } elseif (is_int($value)) {
                     $this->bindValue(':'.$name, $value, PDO::PARAM_INT);
-				} else {
-					$this->bindValue(':'.$name, $value, PDO::PARAM_STR);
-				}
-			}
-		}
-		parent::execute();
-	}
+                } else {
+                    $this->bindValue(':'.$name, $value, PDO::PARAM_STR);
+                }
+            }
+        }
+        parent::execute();
+    }
 }
--- tests/PDOTest.php
+++ tests/PDOTest.php
@@ -4,36 +4,40 @@ require "vendor/autoload.php";
 
 use FFSPHP\PDO;
 
-class PDOTest extends \PHPUnit\Framework\TestCase {
-	var $dsn;
+class PDOTest extends \PHPUnit\Framework\TestCase
+{
+    public $dsn;
 
-	public function setUp(): void {
-		parent::setUp();
+    public function setUp(): void
+    {
+        parent::setUp();
         //$this->dsn = getenv('DSN') ? getenv('DSN') : "sqlite::memory:";
         $this->dsn = getenv('DSN') ? getenv('DSN') : "sqlite:test.sqlite";
-	}
+    }
 
-	public function testConstructor() {
-		$db = new PDO($this->dsn);
-		$this->assertNotNull($db);
-		return $db;
-	}
+    public function testConstructor()
+    {
+        $db = new PDO($this->dsn);
+        $this->assertNotNull($db);
+        return $db;
+    }
 
-	public function testConstructorWithAuth() {
-		try {
-			new PDO("test:user=foo;password=bar");
-			$this->assertTrue(False);
-		}
-		catch(\PDOException $e) {
-			// "can't find driver" is expected
-			$this->assertTrue(True);
-		}
-	}
+    public function testConstructorWithAuth()
+    {
+        try {
+            new PDO("test:user=foo;password=bar");
+            $this->assertTrue(false);
+        } catch (\PDOException $e) {
+            // "can't find driver" is expected
+            $this->assertTrue(true);
+        }
+    }
 
     /**
      * @depends testConstructor
      */
-    public function testBools(PDO $db) {
+    public function testBools(PDO $db)
+    {
         $db->execute("DROP TABLE IF EXISTS bools");
         $db->execute("CREATE TABLE bools (nt BOOLEAN, nf BOOLEAN, pt BOOLEAN, pf BOOLEAN)");
         $db->execute(
@@ -51,7 +55,8 @@ class PDOTest extends \PHPUnit\Framework\TestCase {
     /**
      * @depends testConstructor
      */
-    public function testBaseData(PDO $db) {
+    public function testBaseData(PDO $db)
+    {
         $db->execute("DROP TABLE IF EXISTS test");
         $db->execute("CREATE TABLE test (id INTEGER, textval TEXT, boolval BOOLEAN)");
         $db->execute(
@@ -63,22 +68,24 @@ class PDOTest extends \PHPUnit\Framework\TestCase {
             ["id"=>2, "text"=>"world", "bool"=>false]
         );
         // var_dump($db->execute("SELECT * FROM test")->fetchAll());
-        $this->assertTrue(True);
+        $this->assertTrue(true);
         return $db;
     }
 
     /**
-	 * @depends testBaseData
-	 */
-	public function testExecute(PDO $db) {
-		$val = $db->execute("SELECT * FROM test WHERE id=:id", ["id"=>2])->fetch();
-		$this->assertEquals("world", $val['textval']);
-	}
+     * @depends testBaseData
+     */
+    public function testExecute(PDO $db)
+    {
+        $val = $db->execute("SELECT * FROM test WHERE id=:id", ["id"=>2])->fetch();
+        $this->assertEquals("world", $val['textval']);
+    }
 
-	/**
-	 * @depends testBaseData
-	 */
-	public function testBindingMetaInt(PDO $db) {
+    /**
+     * @depends testBaseData
+     */
+    public function testBindingMetaInt(PDO $db)
+    {
         // By default, mysql would bind this as the string '1' and then
         // throw a syntax error - let's check that that doesn't happen.
         $res = $db->execute("SELECT * FROM test LIMIT :limit", ["limit" => 1])->fetchAll();
@@ -88,26 +95,29 @@ class PDOTest extends \PHPUnit\Framework\TestCase {
     /**
      * @depends testBaseData
      */
-    public function testBindingInt(PDO $db) {
+    public function testBindingInt(PDO $db)
+    {
         $res = $db->execute("SELECT * FROM test WHERE id=:id", ["id"=>2])->fetch();
-		$this->assertEquals("world", $res['textval']);
-	}
+        $this->assertEquals("world", $res['textval']);
+    }
 
-	/**
-	 * @depends testBaseData
-	 */
-	public function testBindingStr(PDO $db) {
-		$res = $db->execute("SELECT * FROM test WHERE textval=:textval", ["textval"=>"hello"])->fetch();
-		$this->assertEquals(1, $res['id']);
-	}
+    /**
+     * @depends testBaseData
+     */
+    public function testBindingStr(PDO $db)
+    {
+        $res = $db->execute("SELECT * FROM test WHERE textval=:textval", ["textval"=>"hello"])->fetch();
+        $this->assertEquals(1, $res['id']);
+    }
 
-	/**
-	 * @depends testBaseData
-	 */
-	public function testBindingBool(PDO $db) {
+    /**
+     * @depends testBaseData
+     */
+    public function testBindingBool(PDO $db)
+    {
         $res = $db->execute("SELECT * FROM test WHERE boolval=:boolval", ["boolval"=>true])->fetch();
         $this->assertEquals(1, $res['id']);
-		$res = $db->execute("SELECT * FROM test WHERE boolval=:boolval", ["boolval"=>false])->fetch();
-		$this->assertEquals(2, $res['id']);
-	}
+        $res = $db->execute("SELECT * FROM test WHERE boolval=:boolval", ["boolval"=>false])->fetch();
+        $this->assertEquals(2, $res['id']);
+    }
 }
